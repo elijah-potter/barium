cab499a3430fa5b7a3fa616d5745fc9876ac0eb1
Elijah Potter <elijah.sirius@protonmail.com>
2022-01-06 11:00:29 -0700

    Made Mat3 available

--- src/lib.rs
+++ src/lib.rs
@@ -5,5 +5,5 @@ pub mod renderers;
 
 pub use canvas::{Canvas, Renderer, Shape, Stroke, LineEnd};
 pub use color::Color;
-pub use glam::{UVec2, Vec2};
+pub use glam::{UVec2, Vec2, Mat2};
 pub use image::RgbaImage;
